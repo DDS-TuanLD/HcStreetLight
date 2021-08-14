9d06d59d231de03cc5db53ffe7bde1c1837ff0ab
tuanldchainos <tuanldhaihaua@gmail.com>
2021-08-14 17:52:52 +0700

    add Constant

--- Constants/Constant.py
+++ Constants/Constant.py
@@ -4,6 +4,8 @@ MQTT_QOS = 2
 MQTT_KEEP_ALIVE = 60
 MQTT_CLOUD_TO_DEVICE_REQUEST_TOPIC = "cloud/device/request"
 MQTT_CLOUD_TO_DEVICE_RESPONSE_TOPIC = "cloud/device/response"
+MQTT_DEVICE_TO_CLOUD_REQUEST_TOPIC = "device/cloud/request"
+MQTT_DEVICE_TO_CLOUD_RESPONSE_TOPIC = "device/cloud/response"
 MQTT_USER = "RD"
 MQTT_PASS = "1"
 
