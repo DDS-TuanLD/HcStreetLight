269a7df5a4a5e697a2d511c95f56c638b08cd59d
tuanldchainos <tuanldhaihaua@gmail.com>
2021-08-16 10:29:01 +0700

    add db models, repos, services

---
+++ Table/Network.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Float
+from sqlalchemy import Table, Column, String, MetaData
+
+
+class Network:
+    def __init__(self, metadata: MetaData):
+        self.network = Table('Network', metadata,
+                             Column('NetworkId', String, primary_key=True, nullable=False),
+                             Column('NetworkType', String),
+                             Column('NetworkKey', String),
+                             )
---
+++ Table/Property.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer
+from sqlalchemy import Table, Column, String, MetaData
+
+
+class Property:
+    def __init__(self, metadata: MetaData):
+        self.property = Table('Property', metadata,
+                              Column('PropertyId', Integer, primary_key=True, nullable=False),
+                              Column('Code', String),
+                              Column('Name', String),
+                              )
