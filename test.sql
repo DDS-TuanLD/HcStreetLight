0bf275f808ae6bbd5d1fc20e35fd5b7e56aa714d
tuanldchainos <tuanldhaihaua@gmail.com>
2021-08-20 22:39:25 +0700

    update smt

--- test.sql
+++ test.sql
@@ -1,2 +1,2 @@
-INSERT INTO EventTriggerOutputDeviceMapping (EventTriggerId, DeviceAddress, IsEnable)
-VALUES(3, "33330db885a3000000008a2e03744444",TRUE);
+INSERT INTO DevicePropertyMapping(DeviceAddress, PropertyId, PropertyValue)
+VALUES("cccc0db885a3000000008a2e03707334", 1, 0)
